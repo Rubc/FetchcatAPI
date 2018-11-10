7faa2f4ed3b78c7e2e033133ec41aca403704e6c
rch <ruben.charlier@gmail.com>
2018-11-10 16:12:37 +0100

    not working

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,32 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class CalendarController
 {
-    /**
-     * @var Environment
-     */
-    private $twig;
-
-    public function __construct(Environment $twig)
+    public function __construct
+    (
+        Environment $twig
+    )
     {
         $this->twig = $twig;
     }
 
     /**
-     * @Route("/calendar", name="calendar")
+     * @Route("/", name="index")
      */
     public function index()
     {
-        return new Response(
-            $this->twig->render('calendar/index.html.twig', [
-                'controller_name' => 'Title'
-            ])
-        );
+        return $this->redirectToRoute('customers');
     }
 }
