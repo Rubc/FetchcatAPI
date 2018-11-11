c633ff9e93747d18743d44ddba1643ca807d08cc
rch <ruben.charlier@gmail.com>
2018-11-11 20:54:59 +0100

    Fix depreciation for Vue

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ Encore
 
     // uncomment if you use Sass/SCSS files
     // .enableSassLoader()
-
+    Encore.enableSingleRuntimeChunk()
     // uncomment for legacy applications that require $/jQuery as a global variable
     // .autoProvidejQuery()
 
