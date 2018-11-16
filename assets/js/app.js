8db82b398fa707e6a74d1e3df0167398790f4800
rch <ruben.charlier@gmail.com>
2018-11-16 18:00:46 +0100

    Vue login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Example from './components/Example'
+import Login from './components/Login'
 
 /**
  * Create a fresh Vue Application instance
+ *  yarn encore dev --watch
  */
 new Vue({
     el: '#app',
-    components: {Example}
+    components: {Example, Login}
 });
