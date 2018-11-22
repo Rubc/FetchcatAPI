00566d300c7015d6b514e7d41562c5dfb830de05
rch <ruben.charlier@gmail.com>
2018-11-22 20:13:36 +0100

    Basic controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BoardRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,15 @@ class IndexController extends AbstractController
      * @var Environment
      */
     private $twig;
+    /**
+     * @var BoardRepository
+     */
+    private $boardRepository;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, BoardRepository $boardRepository)
     {
         $this->twig = $twig;
+        $this->boardRepository = $boardRepository;
     }
 
     /**
