3ebe1f9bbc21fecbe78b76b7d14a2571319be397
rch <ruben.charlier@gmail.com>
2018-12-08 11:31:58 +0100

    Key length update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,13 +14,6 @@ use Doctrine\ORM\Mapping as ORM;
  *              unique   = true
  *          )
  *      ),
- *      @ORM\AttributeOverride(name="emailCanonical",
- *          column=@ORM\Column(
- *              name     = "email_canonical",
- *              length   = 191,
- *              unique   = true
- *          )
- *      )
  * })
  */
 class User extends BaseUser
